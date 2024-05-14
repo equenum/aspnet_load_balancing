4e8caed1aae8a05b704bf7df17a738f46d6326a5
Egor Quenum <georgequenum@yahoo.com>
2024-05-14 23:38:24 +0400

    Implement MediaService.Api controllers

---
+++ MediaService.Api/Controllers/MediaController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediaService.Api;
+
+[ApiController]
+[Route("api/public/[controller]")]
+public class MediaController : ControllerBase
+{
+    [HttpGet("stream")]
+    public IActionResult GetMediaStream()
+    {
+        var bytes = new byte[100];
+        
+        // generate random bytes to imitate a workload
+        var random = new Random();
+        random.NextBytes(bytes);
+
+        return Ok(string.Join(" ", bytes));
+    }
+}
---
+++ MediaService.Api/Controllers/UserPreferencesController.cs
@@ -0,0 +1,16 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MediaService.Api;
+
+[ApiController]
+[Route("api/public/[controller]")]
+public class UserPreferencesController : ControllerBase
+{
+    [HttpGet("lang")]
+    public async Task<IActionResult> GetLanguageAsync()
+    {
+        await Task.Delay(1000);
+        return Ok("English");
+    }
+}
