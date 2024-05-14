c74d52173ffbde434e630353cf493c20373d33a8
Egor Quenum <georgequenum@yahoo.com>
2024-05-15 02:58:22 +0400

    Add authentication and session affinity

--- Yarp.Gateway/Program.cs
+++ Yarp.Gateway/Program.cs
@@ -1,6 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Claims;
+using System.Text.Json;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Yarp.ReverseProxy.Transforms;
 
 namespace Yarp.Gateway;
 
@@ -15,7 +23,30 @@ public class Program
         builder.Services.AddSwaggerGen();
 
         builder.Services.AddReverseProxy()
-            .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"));
+            .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
+            .AddTransforms(builderContext => 
+            {
+                if (!string.IsNullOrEmpty(builderContext.Route.AuthorizationPolicy)) 
+                {
+                    builderContext.AddRequestTransform(requestContext => 
+                    {
+                        var userClaims = requestContext.HttpContext.User.Claims.Aggregate(
+                            new Dictionary<string, string>(),
+                                (dict,claim) =>
+                                {
+                                    dict[claim.Type] = claim.Value;
+                                    return dict;
+                                });
+
+                        requestContext.ProxyRequest.Headers.Add("x-auth-json", JsonSerializer.Serialize(userClaims));
+                        return ValueTask.CompletedTask;
+                    });
+                }
+            });
+
+        // AddReverseProxy() already adds the default authorization under the hood
+        builder.Services.AddAuthentication("cookie")
+            .AddCookie("cookie");
 
         builder.Services.AddHealthChecks();
 
@@ -26,11 +57,33 @@ public class Program
             app.UseSwagger();
             app.UseSwaggerUI();
         }
+        
+        app.MapGet("/login/{userName}", (string userName) => Results.SignIn(
+            new ClaimsPrincipal(
+                new ClaimsIdentity(
+                    new Claim[] 
+                    { 
+                        new Claim("id", Guid.NewGuid().ToString()),
+                        new Claim("userName", userName) 
+                    },
+                    "cookie"
+                )
+            ),
+            authenticationScheme: "cookie"
+        ));
+
+        // get all the current claims for testing
+        app.MapGet("/claims", (ClaimsPrincipal cp) => 
+            cp.Claims.Aggregate(
+                new Dictionary<string, string>(),
+                (dict,claim) =>
+                {
+                    dict[claim.Type] = claim.Value;
+                    return dict;
+                }));
 
         app.MapReverseProxy();
         app.MapHealthChecks("health");
-
-        app.MapControllers();
 
         app.Run();
     }
